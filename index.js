3433e939ad5dea8004cb5f34f297459083774297
Your Name <manahilrasheed507@gmail.com>
2024-04-25 04:35:27 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+let IsHandsome = true;
+let IsRich = false;
+if (IsHandsome && IsRich) {
+    console.log("logic true");
+}
+else {
+    console.log("logic false");
+}
+export {};
---
+++ index.ts
@@ -0,0 +1,9 @@
+#! /usr/bin/env node
+let IsHandsome = true;
+let IsRich = false;
+
+if (IsHandsome && IsRich){
+    console.log("logic true")
+}else {
+    console.log("logic false")
+}
